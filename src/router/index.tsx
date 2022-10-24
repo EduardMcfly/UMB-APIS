97990c6a1e4bc903d9d5ab2ad4d0cec3a9f78831
Castellanos, Eduard Andres (Colombia) <andrescastellanos112013@gmail.com>
2022-10-23 21:30:56 -0500

    chore: Changed createBrowserRouter to createHashRouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, Link } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
 import Maps from '../pages/Maps';
 import Main from '../pages/Main';
 import Pokemons from '../pages/Pokemons';
 import StarWars from '../pages/StarWars';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
     element: <Main />,
